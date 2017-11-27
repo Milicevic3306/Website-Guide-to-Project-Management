cad229fb68cb2b101ad8e00a8bbd3b7628ae2339
Milicevic3306 <33694508+Milicevic3306@users.noreply.github.com>
2017-11-27 02:05:14 +0100

    Add All files

---
+++ question.js
@@ -0,0 +1,114 @@
+
+var questions = [{
+  "question": "Which statement best describes Scrum?",
+  "option1": "A complete methodology that defines how to develop software",
+  "option2": "A cookbook that defines best practices for software development",
+  "option3": "A defined and predictive process that fits in with traditional management approaches",
+  "option4": "A framework with built-in reality checks for complex work in uncertain domains",
+  "answer": "4"
+},
+ {
+  "question": "Who estimates the effort to complete a product backlog item?",
+  "option1": "The Scrum Development Team, after clarifying the requirement",
+  "option2": "The Scrum Master, with input from the scrum development tea",
+  "option3": "The most senior people in the organization, such as engineering managers and chief architect",
+  "option4": "The product owner, with input from the scrum development team",
+  "answer": "1"
+}, {
+  "question": "How is Product Backlog Arranged?",
+  "option1": " Items are randomly arranged",
+  "option2": "Into categories, P1, P2, P3 etc.",
+  "option3": "Large items at the top, small items at the bottom",
+  "option4": "Most important items at the top, least important items at the bottom",
+  "answer": "4"
+
+}, {
+  "question":"Which is not an effective way to encourage collaboration between the scrum dvevelopment team and product owner?",
+  "option1": "Act as a go-between for them",
+  "option2": "Teach the team to talk in terms of business needs and objectives",
+  "option3": "Listen to their communications",
+  "option4": "Teach the product owner about technologies the team uses",
+  "answer": "1"
+}, {
+    "question":"Which of the following is the scrum team NOT responsible for?",
+  "option1": "Planning how to meet a sprint goal",
+  "option2": "Increasing productivity and learning",
+  "option3": "Resolving internal conflicts",
+  "option4": "Selecting the product owner",
+  "answer": "4"
+
+}, {
+  "question": "In a Scrum organization, which is a responsibility of management external to the Scrum team?",
+  "option1": "Coordinate activities between Scrum teams",
+  "option2": "Monitor the daily scrum meetings and reward team players",
+  "option3": "Help resolve team-reported impediments and improve the larger organization",
+  "option4": "Setup performance appraisals",
+  "answer": "3"
+
+}, {
+  "question": "When is the sprint backlog created?",
+  "option1": "Whenever needed",
+  "option2": " During the backlog refinement meeting",
+  "option3": "During the sprint planning meeting",
+  "option4": "At the beginning of the project",
+  "answer": "3"
+}, {
+    "question": "When is Sprint execution completed?",
+  "option1": " When all tasks are complete",
+  "option2": " When the time-box expires",
+  "option3": " When all committed product backlog items meet their definition of done",
+  "option4": "It depens",
+  "answer": "2"
+
+}, {
+  "question": "When should a retrospective meeting be held?",
+  "option1": "At the end of each sprint, before the sprint review meeting",
+  "option2": "Only at the end of a project",
+  "option3": "Every now and then, when the team wants to improve",
+  "option4": "At the end of each sprint, after the sprint review meeting",
+  "answer": "4"
+}, {
+  "question": "What is the recommended size of a scrum team?",
+  "option1": " It doesn't matter, as long as the team is cross functional",
+  "option2": "10 plus or minus 3, or more if the team is geographically dispersed",
+  "option3": "7 plus or minus 2",
+  "option4": "9 plus or minus 3",
+  "answer": "3"
+},{
+  "question": "What should happen in the sprint review meeting?",
+  "option1": " A report about what happened during the Sprint",
+  "option2": "Feedback from stakeholders resulting in additional items added to the product backlog",
+  "option3": "Declration by the product owner which items are done and which ones are not done",
+  "option4": "Live demonstration of a potentially shippable product increment",
+  "answer": "4"
+
+},{
+
+  "question": "Which one of the following is not a Scrum role?",
+  "option1": "Product Owner",
+  "option2": "Scrum Master",
+  "option3": "Product Manager",
+  "option4": "Team",
+  "answer": "3"
+},{
+  "question": "What is the duration of a sprint in Scrum??",
+  "option1": " 4 weeks",
+  "option2": "30 days",
+  "option3": "28-30 days",
+  "option4": "6-8 weeks",
+  "answer": "2"
+},{
+  "question": "Which of the following questions is not relevant for regular Scrum Meeting?",
+  "option1": " What have you done since the last Daily Scrum regarding this project?",
+  "option2": "What will you do between now and the next Daily Scrum meeting regarding this project?",
+  "option3": "Would you be able to finish your work on time?",
+  "option4": "What impedes you from performing your work as effectively as possible?",
+  "answer": "3"
+},{
+  "question": "During first Sprint - What does the Team do?",
+  "option1": "Implement and accomplish the Sprint goal",
+  "option2": "Creates User specifications, design and other documents",
+  "option3": "Decides on and describes the architecture and infrastructure",
+  "option4": "Makes a project and release plan for the coming Sprints",
+  "answer": "1"
+}];
